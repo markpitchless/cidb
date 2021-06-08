dc81e907a0c9671f2c2a889887365a2409482b37
Mark Pitchless <markpitchless@gmail.com>
2021-06-08 21:39:35 +0100

    Log control env vars and Sloppy::DSL

--- lib/cidb/sloppy.rb
+++ lib/cidb/sloppy.rb
@@ -1,35 +1,70 @@
 #!/usr/bin/env ruby
 #vim: sw=2 ts=2 sts=2:
 
+require 'logger'
 require 'slop' # option parsing
 
 module CIDB
   # Mixin for classes that act as command lines
   module Sloppy
-    # TODO: 
-    # attr :banner
-    # def self.banner= DSL that sets up the banner
-    # e.g.banner "usage: $0 [OPTIONS] [FILES]"
+    module DSL
+      def banner(desc=nil)
+        if block_given?
+          define_method :banner, block
+        else
+          define_method :banner do
+            desc.to_s % { prog: prog_name }
+          end
+        end
+      end
 
-    # TODO: def description(String|Block) ## Generate a description method
+      def description(desc=nil)
+        if block_given?
+          define_method :description, block
+        else
+          define_method :description do desc.to_s end
+        end
+      end
+    end
+    def self.included(base) base.extend DSL end
 
     def prog_name
       @progname ||= File.basename $0
     end
 
-    ## Log info messages to STDERR
-    def info(msg)
-      STDERR.puts "#{@progname}:  INFO: #{msg}"
+    ##
+    # Return the Logger instance, creates a new one (and caches) if we don't
+    # already have one. Default logger is STDERR.
+    # You can set CIDB_LOG to change. Use: STDOUT, STDERR or a file name.
+    # that.
+    def logger
+      @logger ||= begin
+        log_to = ENV.fetch "CIDB_LOG", "STDERR"
+        fmt    = ENV.fetch "CIDB_LOG_FORMAT", "%{prog}: %{level} - %{msg}\n"
+        if log_to.match?(/^STD[^\s]+$/i)
+          log_to = File.const_get log_to.upcase
+        end
+        log = Logger.new log_to
+        log.formatter = proc do |severity, datetime, progname, msg|
+          fmt % { level: severity, time: datetime, prog: prog_name, msg: msg }
+        end
+        log
+      end
     end
 
-    ## Log error messages to STDERR
+    ## Log info messages
+    def info(msg)
+      logger.info msg
+    end
+
+    ## Log error messages
     def error(msg)
-      STDERR.puts "#{@progname}: ERROR: #{msg}"
+      logger.error msg
     end
 
     ## Log error message and exit with the code given
     def fail!(status, msg)
-      STDERR.puts "#{@progname}:  FAIL: #{msg}"
+      logger.fatal msg
       exit status
     end
 
@@ -40,6 +75,7 @@ module CIDB
     # TODO: --quite to stop info logging. Twice for errors. 3 times for --silent
     def std_slop(opt)
       opt.banner = banner if respond_to? :banner
+
       opt.on '--version', 'Print the version' do
         puts CIDB::VERSION
         exit
@@ -54,7 +90,7 @@ module CIDB
         opt.separator "    " + description
       end
 
-      yield(opt) if block_given?
+      yield opt if block_given?
     end
 
     ##
@@ -64,6 +100,11 @@ module CIDB
       std_slop opt
     end
 
+    ##
+    # Run the command.
+    # Parse the options, calling slop(opt). Fix up ARGV.
+    # Call your main(opt, arg) method (if there). Hook this to write your command.
+    # Slop errors (opt parsing errors) will log the fatal and exit 1.
     def run
       @opts = Slop.parse { |opt| slop opt }
       @args = @opts.arguments
