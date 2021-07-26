57dd7a114768ca8d90915c9790eb72375ec97efe
Mark Pitchless <markpitchless@gmail.com>
2021-07-26 14:03:37 +0100

    Add pkeys for test suites and cases
    Add the keys and skip insert on duplicates for suites and cases.

--- lib/cidb/junit/db_writer.rb
+++ lib/cidb/junit/db_writer.rb
@@ -3,7 +3,14 @@
 
 module CIDB
   module JUnit
+    ##
+    # JUnit handler to write suites and cases to the database.
+    #
+    # Fills the test_suites and test_cases tables. If rows already exist the new
+    # insert is skipped. Re-running does not produce duplicates.
     class DBWriter
+      # TODO: start and finish events, keep count and log inserts done
+
       def initialize(id)
         @build_id = id
         @current_suite = nil
@@ -14,7 +21,7 @@ module CIDB
       end
 
       def end_suite(s)
-        CIDB::DB[:test_suites].insert({
+        CIDB::DB[:test_suites].insert_conflict.insert({
           build_id: @build_id.to_s,
           name: s.name.to_s,
           timestamp: s.timestamp,
@@ -27,7 +34,7 @@ module CIDB
       end
 
       def on_case(c)
-        CIDB::DB[:test_cases].insert ({
+        CIDB::DB[:test_cases].insert_conflict.insert({
           build_id: @build_id,
           suite_name: @current_suite&.name.to_s,
           classname: c.classname.to_s,
